4d2ba923f582d6ec3fb2222b570cfe797d71da04
gitjona <89576059+gitjona@users.noreply.github.com>
2021-12-09 15:55:17 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,29 +9,35 @@ import Trainings from './components/Trainings';
 
 function App() 
 {
+  const d = new Date();
+  const year = d.getFullYear();
+
   return (
     <div className="App">
-
       <BrowserRouter>
-      <AppBar postion="static">
-        <Toolbar>
-          <Typography variant="h6">
-            Finaltask
-            <br></br>
-            <Link to="/" style={{color: "#33ccff"}}>Customers</Link><br></br>
-            <Link to="/trainings" style={{color: "#33ccff"}}>Trainings</Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <br></br><br></br><br></br><br></br>
+        <AppBar postion="static">
+          <div align="right">
+            <small style={{color: "#3399ff"}}>&copy; 2021 - {year} Jona Oksanen</small>
+          </div>
+          <Toolbar>
+            <Typography variant="h6">
+              Finaltask
+              <br></br>
+              <Link to="/" style={{color: "#33ccff"}}>Customers</Link><br></br>
+              <Link to="/trainings" style={{color: "#33ccff"}}>Trainings</Link>
+            </Typography>          
+          </Toolbar>
+        </AppBar>
+
+        <br></br><br></br><br></br><br></br>
         <Routes>
           <Route exact path="/" element={<Customers/>} />
           <Route path="/trainings" element={<Trainings/>} />
           <Route path="*" element={<notFound/>}/>
         </Routes>
-      
       </BrowserRouter>
     </div>
+    
   );
 }
 
